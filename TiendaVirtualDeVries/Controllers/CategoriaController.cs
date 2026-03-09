98b26a35efc26b474813f4f23ef44b002fbf3f70
Jossa7077 <santyjossa5@gmail.com>
2026-03-09 17:10:56 -0500

    Agregar CategoriaController y Index.cshtml en Views

---
+++ TiendaVirtualDeVries/Controllers/CategoriaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TiendaVirtualDeVries.Models;
+
+namespace TiendaVirtualDeVries.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categorias = new List<Categoria>
+            {
+                new Categoria{ Id = 2, Nombre = "Ropa", Descripcion = "Ropa para damas y caballeros"},
+                new Categoria{ Id = 2, Nombre = "Celulares", Descripcion = "Varios tipos de celulares"},
+                new Categoria{ Id = 2, Nombre = "Televisores", Descripcion = "Varios tipos de televisores"},
+                new Categoria{ Id = 3, Nombre = "", Descripcion = "No definido"}
+            };
+            return View(categorias);
+        }
+    }
+}
